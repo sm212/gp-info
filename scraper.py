2045bff6d2704362a2defd50093b122cf8207544
Sean <sean.maguire212@gmail.com>
2019-12-14 16:10:11 +0000

    added outline for scraper

---
+++ scraper.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests as r
+
+def get_practice_ids():
+    "Returns a list of GP practice IDs from the NHS site"
+    pass
+
+def get_overview(practice_id):
+	"Get overview information for practice ID"
+	pass
+
+def get_reviews(practice_id, include_response = True):
+	"Get all reviews for practice ID"
+	pass
