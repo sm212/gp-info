9a07347f143345b104192cc3dab22628d894a2ae
Sean <sean.maguire212@gmail.com>
2019-12-14 16:48:50 +0000

    added parse_key_info function

--- scraper.py
+++ scraper.py
@@ -28,6 +28,23 @@ def get_overview(practice_id):
 
 	return(overview_info)
 
+def parse_key_info(soup):
+    "Parse data from the 'Key Information' box of an overview page"
+
+    key_info_data = [tag.contents[0] for tag in soup.find_all(class_ = 'indicator-value')]
+    key_info_keys = ['patients', 'evening_weekend', 'would_rec']
+    key_info = {key: data for key, data in zip(key_info_keys, key_info_data)}
+
+    # Fix datatypes & add in survey response numbers
+    key_info['patients'] = int(key_info['patients'])
+    key_info['would_rec'] = int(key_info['would_rec']) / 100
+    key_info['n_asked_rec'] = int(soup.find_all(class_ = 'indicator-text')[-1].contents[0].split()[-2])
+    
+    return(key_info)
+
+def parse_doctor_info(soup):
+	"Parse data from the 'Doctors' box of an overview page"
+	pass
 
 def get_reviews(practice_id, include_response = True):
 	"Get all reviews for practice ID"
