01717fe0d35111e35a619191d886cf7d4c043cba
Sean <sean.maguire212@gmail.com>
2019-12-14 16:20:10 +0000

    get_practice_ids

--- scraper.py
+++ scraper.py
@@ -3,7 +3,14 @@ import requests as r
 
 def get_practice_ids():
     "Returns a list of GP practice IDs from the NHS site"
-    pass
+
+    page = r.get('https://www.nhs.uk/Services/Pages/HospitalList.aspx?chorg=GpBranch')
+    soup = BeautifulSoup(page.content)
+    
+    links = [link.get('href') for link in soup.find_all('a')]
+    ids = [link.split('=')[1] for link in links if '=' in link]
+    
+    return(ids)
 
 def get_overview(practice_id):
 	"Get overview information for practice ID"
